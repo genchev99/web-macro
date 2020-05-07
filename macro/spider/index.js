4d3f5b7a91ee81830d7a0f24e32bd131c11d2f51
Hristiyan Genchev <genchev99@users.noreply.github.com>
2020-05-07 20:01:04 +0300

    log event for now

--- macro/spider/index.js
+++ macro/spider/index.js
@@ -75,30 +75,18 @@ const href = async (page, commandArguments) =>
 
 
 exports.handler = async (event) => {
-  const executablePath = event.isOffline
-    ? './node_modules/puppeteer/.local-chromium/mac-674921/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
-    : await chromium.executablePath;
+  // const executablePath = event.isOffline
+  //   ? './node_modules/puppeteer/.local-chromium/mac-674921/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+  //   : await chromium.executablePath;
+  //
+  // const browser = await puppeteer.launch({
+  //   args: chromium.args,
+  //   executablePath
+  // });
+  //
+  // const page = await browser.newPage();
+  //
 
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto("https://www.google.com", {
-    waitUntil: ["networkidle0", "load", "domcontentloaded"]
-  });
-
-  const pdfStream = await page.pdf();
-
-  return {
-    statusCode: 200,
-    isBase64Encoded: true,
-    headers: {
-      "Content-type": "application/pdf"
-    },
-    body: pdfStream.toString("base64")
-  };
+  console.log(event);
 };
 
