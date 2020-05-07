a478394de97b5700cebe415e7d4c3554ea913041
Hristiyan Genchev <genchev99@users.noreply.github.com>
2020-05-07 19:52:09 +0300

    Commands parser and validator

--- macro/spider/index.js
+++ macro/spider/index.js
@@ -1,9 +1,43 @@
-const chromium = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+'use strict';
+
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+const validateCommand = (commandArguments) => {
+  const commandType = commandArguments.shift();
+
+  return commandType === 'click' && commandArguments.length >= 1
+    || commandType === 'type' && commandArguments.length >= 2
+    || commandType === 'screenshot' && commandArguments.length >= 0
+    || commandType === 'goto' && commandArguments.length >= 1
+    || commandType === 'pdf' && commandArguments.length >= 0
+    || commandType === 'textContent' && commandArguments.length >= 1
+    || commandType === 'attribute' && commandArguments.length >= 1
+    || commandType === 'href' && commandArguments.length >= 1;
+};
+
+const parseCommand = async (page, command) => {
+  const parsedCommand = command.split(' ');
+
+  if (validateCommand(parsedCommand.slice())) {
+    const commandType = parsedCommand.shift();
+
+    await commands[commandType](page, parsedCommand);
+  }
+};
+
+const commands = {
+  click,
+};
+
+const click = async (page, commandArguments) =>
+  await page.click(commandArguments[0]);
+
+
 
 exports.handler = async (event) => {
   const executablePath = event.isOffline
-    ? "./node_modules/puppeteer/.local-chromium/mac-674921/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
+    ? './node_modules/puppeteer/.local-chromium/mac-674921/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
     : await chromium.executablePath;
 
   const browser = await puppeteer.launch({
